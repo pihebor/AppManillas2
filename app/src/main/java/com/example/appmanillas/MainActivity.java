dace920d110803e85a4b8278c263caec528e1d7d
Pedro Hernandez <pihebor@hotmail.com>
2020-05-25 21:46:55 -0500

    Terminada Interfaz de MAterial, se hace la traduccion al Ingles en el string y se obtienen los datos del combo sin recorrerlo

--- app/src/main/java/com/example/appmanillas/MainActivity.java
+++ app/src/main/java/com/example/appmanillas/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.appmanillas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView resultado;
+    private Spinner combo_material, combo_dije, combo_tipo_dije;
+    private String[] material;
+    private String[] dije;
+    private String[] tipo_dije;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        combo_material = findViewById(R.id.cmbMaterial);
+        material = getResources().getStringArray(R.array.material);
+
+
     }
 }
